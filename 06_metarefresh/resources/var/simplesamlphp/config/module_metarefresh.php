55cb9b7d2057365c39fb9265bc9f9dc1a29e391b
Frank Tröger <frank.troeger@fau.de>
2025-01-14 11:13:47 +0100

    Update step 06

--- 06_metarefresh/resources/var/simplesamlphp/config/module_metarefresh.php
+++ 06_metarefresh/resources/var/simplesamlphp/config/module_metarefresh.php
@@ -1,38 +1,72 @@
 <?php
 
 $config = [
-	'sets' => [
+    /*
+     * Global blacklist: entityIDs that should be excluded from ALL sets.
+     */
+    #'blacklist' = array(
+    #    'http://my.own.uni/idp'
+    #),
+
+    /*
+     * Conditional GET requests
+     * Efficient downloading so polling can be done more frequently.
+     * Works for sources that send 'Last-Modified' or 'Etag' headers.
+     * Note that the 'data' directory needs to be writable for this to work.
+     */
+    #'conditionalGET' => true,
+
+    'sets' => [
+
         'dfntest' => [
-            'cron'		=> ['hourly'],
-            'sources'	=> [
+            'cron' => ['hourly'],
+            'sources' => [
                 [
+                    /*
+                     * entityIDs that should be excluded from this src.
+                     */
+                    #'blacklist' => array(
+                    #    'http://some.other.uni/idp',
+                    #),
+
+                    /*
+                     * Whitelist: only keep these EntityIDs.
+                     */
+                    #'whitelist' => array(
+                    #    'http://some.uni/idp',
+                    #    'http://some.other.uni/idp',
+                    #),
+
+                    #'conditionalGET' => true,
                     'src' => 'http://www.aai.dfn.de/fileadmin/metadata/dfn-aai-test-metadata.xml',
                     'certificates' => ['dfn-aai.pem'],
-                    //'validateFingerprint' => 'cbf57ce9e8b1bf2abd0605bd943a0ce505829325',
                     'template' => [
                         'tags' => ['dfntest'],
                     ],
                 ],
             ],
-            'expireAfter'   => 60*60*24*4, // Maximum 4 days cache time.
-            'outputDir' 	=> 'metadata/dfntest/',
-            'outputFormat'  => 'flatfile',
+
+            'expireAfter' => 34560060, // Maximum 4 days cache time (3600*24*4)
+            'outputDir' => 'metadata/dfntest/',
+            'outputFormat' => 'flatfile',
+            'types' => ['saml20-idp-remote'],
         ],
-		'dfn' => [
-			'cron'		=> ['hourly'],
-			'sources'	=> [
+        'dfn' => [
+            'cron' => ['hourly'],
+            'sources' => [
                 [
-					'src' => 'http://www.aai.dfn.de/fileadmin/metadata/dfn-aai-basic-metadata.xml',
+                    'src' => 'http://www.aai.dfn.de/metadata/dfn-aai-idp-metadata.xml',
                     'certificates' => ['dfn-aai.pem'],
-					//'validateFingerprint' => 'cbf57ce9e8b1bf2abd0605bd943a0ce505829325',
-					'template' => [
-						'tags'	    => ['dfn'],
-					],
-				],
-			],
-			'expireAfter' 	=> 60*60*24*4, // Maximum 4 days cache time.
-			'outputDir' 	=> 'metadata/dfn/',
-			'outputFormat'  => 'flatfile',
-		],
-	],
+                    'template' => [
+                        'tags' => ['dfn'],
+                    ],
+                ],
+            ],
+
+            'expireAfter' => 34560060, // Maximum 4 days cache time (3600*24*4)
+            'outputDir' => 'metadata/dfn/',
+            'outputFormat' => 'flatfile',
+            'types' => ['saml20-idp-remote'],
+        ],
+    ],
 ];
